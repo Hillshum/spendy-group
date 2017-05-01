ac53e96206858fc27d46a2f3c256ccd5bcdede53
Hilton Shumway <hillshum@gmail.com>
2017-04-30 19:47:01 -0600

    Add datepicker and wire state up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
+
+import Transaction from './components/transaction';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      memo: "The best place",
+      date: moment(),
+      amount: 1232,
+    }
+
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -10,9 +25,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Transaction memo={this.state.memo}
+          amount={this.state.amount}
+          date={this.state.date}
+          onAmountChange={(amount)=>{this.setState({amount:amount})}}
+          onMemoChange={(memo)=>{this.setState({memo:memo})}}
+          onDateChange={(date)=>(this.setState({date:date}))}
+           />
+
       </div>
     );
   }
--- src/components/transaction/index.js
+++ src/components/transaction/index.js
@@ -1,4 +1,24 @@
-import {Component} from 'react';
+import React from 'react';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 
-class Transaction extends Component 
+export default props => {
+  const {memo, date, amount, onAmountChange, onMemoChange, onDateChange} = props;
+
+  // The datepicker exports just the change, but the others need to be
+  // wrapped with only the value
+  const amountChange = (e) => {
+    onAmountChange(e.target.value);
+  }
+  const memoChange = (e) => {
+    onMemoChange(e.target.value);
+  }
+  return <div className="transaction">
+      <DatePicker selected={date} onChange={onDateChange} />
+      <input type="text" onChange={memoChange} className="memo" value={memo} />
+      <input type="text" onChange={amountChange} className="amount" value={amount} />
+      <input type="text" readOnly className="shared-amount" value={amount/3.0} />
+    </div>
+}
